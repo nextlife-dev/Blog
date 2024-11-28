9b8636e36f4208682846114e5af00c61d25ed766
nextlife-dev <gntodtndls156@gmail.com>
2024-11-28 23:18:58 +0900

    Quartz sync: Nov 28, 2024, 11:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "ko-KR",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "attachments"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
